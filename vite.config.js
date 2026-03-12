5ee2953537850ceacc0a2c01b9bb482fc0bbf127
lank2 <lanvkuk2@gmail.com>
2026-03-12 07:49:12 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3005,
+    port: 3006,
     host: '0.0.0.0',
     allowedHosts: ['phulonghotels.com', '0.0.0.0'],
   },
