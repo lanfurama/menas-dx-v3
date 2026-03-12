c167ded2a74bd8ec6546ed0c359e244813c745b4
lank2 <lanvkuk2@gmail.com>
2026-03-12 07:45:25 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,5 @@ export default defineConfig({
     port: 3005,
     host: '0.0.0.0',
     allowedHosts: ['phulonghotels.com', '0.0.0.0'],
-    proxy: {
-      '/api/v1': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/v1/, '/api/v1'),
-      },
-    },
   },
 })
