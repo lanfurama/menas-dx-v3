1f3d84ab482722dbdadd6fdea41bca04f0cc4e06
lank2 <lanvkuk2@gmail.com>
2026-03-12 07:39:29 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3005, host: '0.0.0.0' },
+  server: {
+    port: 3005,
+    host: '0.0.0.0',
+    allowedHosts: ['phulonghotels.com', '0.0.0.0'],
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/v1/, '/api/v1'),
+      },
+    },
+  },
 })
